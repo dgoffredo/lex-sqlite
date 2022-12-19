12360b0231c7c41817ddad8abeba93e27af4f1b6
David Goffredo <dmgoffredo@gmail.com>
2022-12-18 23:54:07 -0500

    extern "C"

--- lex_sqlite.h
+++ lex_sqlite.h
@@ -10,13 +10,20 @@ typedef enum {
 } lex_sqlite_token_kind_t;
 
 #ifdef __cplusplus
-extern "C"
+#define EXTERN_C extern "C"
+#else
+#define EXTERN_C
 #endif
-    void
-    lex_sqlite(const char *sql, size_t length,
-               void (*on_token)(void *cookie, lex_sqlite_token_kind_t kind,
-                                const char *token_begin, size_t token_length,
-                                const char *inside_begin, size_t inside_length),
-               void *cookie);
 
+EXTERN_C
+void lex_sqlite(const char *sql, size_t length,
+                void (*on_token)(void *cookie, lex_sqlite_token_kind_t kind,
+                                 const char *token_begin, size_t token_length,
+                                 const char *inside_begin,
+                                 size_t inside_length),
+                void *cookie);
+
+EXTERN_C
 char *lex_sqlite_normalize(const char *sql, size_t length, char *output);
+
+#undef EXTERN_C
